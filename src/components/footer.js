b0a42911222d7e5fd30c560f0d8246dbc591c1b6
Jorge Mancero <jman0829@gmail.com>
2020-03-06 11:49:57 -0500

    files updated

---
+++ src/components/footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer className='page-footer black'>
+			<div className='container'>
+				<div className='row'>
+					<div className='col l7 s12'>
+						<h5 className='red-text'></h5>
+						<ul>
+							
+							
+						</ul>
+					</div>
+					
+				</div>
+			</div>
+			<div className='footer-copyright'>
+				<div className='container'>
+					<span className='red-text'></span>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
